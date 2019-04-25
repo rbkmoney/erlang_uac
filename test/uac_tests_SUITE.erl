534c1a3598d2c33745469bda8ff459231d038a3e
Kehitt <devstealth@gmail.com>
2019-04-25 17:49:55 +0300

    Tests for unknown resources

--- test/uac_tests_SUITE.erl
+++ test/uac_tests_SUITE.erl
@@ -1,34 +1,28 @@
 -module(uac_tests_SUITE).
 
+-include_lib("stdlib/include/assert.hrl").
 -include_lib("common_test/include/ct.hrl").
 -include_lib("jose/include/jose_jwk.hrl").
 
 -export([all/0]).
--export([groups/0]).
 -export([init_per_suite/1]).
 -export([end_per_suite/1]).
--export([init_per_group/2]).
--export([end_per_group/2]).
--export([init_per_testcase/2]).
--export([end_per_testcase/2]).
 
 -export([
     successful_auth_test/1,
     invalid_permissions_test/1,
     bad_token_test/1,
     no_token_test/1,
-
     force_expiration_test/1,
     force_expiration_fail_test/1,
-
     bad_signee_test/1,
-
-    different_issuers_test/1
+    different_issuers_test/1,
+    unknown_resources_ok_test/1,
+    unknown_resources_fail_encode_test/1
 ]).
 
 -type test_case_name()  :: atom().
 -type config()          :: [{atom(), any()}].
--type group_name()      :: atom().
 
 -define(expire_as_of_now, #{
     check_expired_as_of => genlib_time:unow()
@@ -42,59 +36,21 @@
     [test_case_name()].
 all() ->
     [
-        {group, general_tests},
-        {group, different_issuers}
-    ].
-
--spec groups() ->
-    [{group_name(), list(), [test_case_name()]}].
-groups() ->
-    [
-        {general_tests, [],
-            [
-                successful_auth_test,
-                invalid_permissions_test,
-                bad_token_test,
-                no_token_test,
-                force_expiration_test,
-                force_expiration_fail_test,
-                bad_signee_test
-            ]
-        },
-        {different_issuers, [],
-            [
-                different_issuers_test
-            ]
-        }
+        successful_auth_test,
+        invalid_permissions_test,
+        bad_token_test,
+        no_token_test,
+        force_expiration_test,
+        force_expiration_fail_test,
+        bad_signee_test,
+        unknown_resources_ok_test,
+        unknown_resources_fail_encode_test,
+        different_issuers_test
     ].
 
 -spec init_per_suite(config()) ->
     config().
 init_per_suite(Config) ->
-    Config.
-
--spec init_per_group(group_name(), config()) ->
-    config().
-init_per_group(general_tests, Config) ->
-    Apps = [
-        genlib_app:start_application(snowflake),
-        genlib_app:start_application(uac)
-    ],
-    uac:configure(#{
-        jwt => #{
-            keyset => #{
-                test => {pem_file, get_keysource("keys/local/private.pem", Config)}
-            }
-        },
-        access => #{
-            service_name => <<"test">>,
-            resource_hierarchy => #{
-                test_resource => #{}
-            }
-        }
-    }),
-    [{apps, Apps}] ++ Config;
-init_per_group(different_issuers, Config) ->
     Apps = [
         genlib_app:start_application(snowflake),
         genlib_app:start_application(uac)
@@ -114,24 +70,9 @@ init_per_group(different_issuers, Config) ->
     }),
     [{apps, Apps}] ++ Config.
 
--spec init_per_testcase(test_case_name(), config()) ->
-    config().
-init_per_testcase(_Name, Config) ->
-    Config.
-
 -spec end_per_suite(config()) ->
     _.
 end_per_suite(Config) ->
-    Config.
-
--spec end_per_group(group_name(), config()) ->
-    _.
-end_per_group(_Name, Config) ->
-    [application:stop(App) || App <- ?config(apps, Config)].
-
--spec end_per_testcase(test_case_name(), config()) ->
-    _.
-end_per_testcase(_Name, Config) ->
     Config.
 
 %%
@@ -191,7 +132,7 @@ bad_signee_test(_) ->
     _.
 different_issuers_test(_) ->
     {ok, Token} = issue_token(?test_service_acl(write), unlimited),
-    uac:configure(#{
+    ok = uac:configure(#{
         jwt => #{},
         access => #{
             service_name => <<"SOME_OTHER_SERVICE">>,
@@ -202,6 +143,31 @@ different_issuers_test(_) ->
     }),
     {ok, {_, {_, []}, _}} = uac:authorize_api_key(<<"Bearer ", Token/binary>>, #{}).
 
+-spec unknown_resources_ok_test(config()) ->
+    _.
+unknown_resources_ok_test(_) ->
+    ok = uac_conf:configure(#{
+        resource_hierarchy => #{
+            different_resource           => #{},
+            test_resource                => #{},
+            even_more_different_resource => #{}
+        }
+    }),
+    ACL = [{[different_resource], read}, {[test_resource], write}, {[even_more_different_resource], read}],
+    {ok, Token} = issue_token(ACL, unlimited),
+    ok = uac_conf:configure(#{
+        resource_hierarchy => #{
+            test_resource => #{}
+        }
+    }),
+    {ok, AccessContext} = uac:authorize_api_key(<<"Bearer ", Token/binary>>, #{}),
+    ok = uac:authorize_operation(?test_service_acl(write), AccessContext).
+
+-spec unknown_resources_fail_encode_test(config()) ->
+    _.
+unknown_resources_fail_encode_test(_) ->
+    ACL = [{[different_resource], read}, {[test_resource], write}, {[even_more_different_resource], read}],
+    ?assertError({badarg, {resource, _}}, issue_token(ACL, unlimited)).
 %%
 
 issue_token(ACL, LifeTime) ->
