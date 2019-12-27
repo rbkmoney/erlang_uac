a80b3e44c562a5b59222b62f7a8a4aed725eb0b6
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-12-27 18:13:48 +0300

    Prettify test

--- test/uac_tests_SUITE.erl
+++ test/uac_tests_SUITE.erl
@@ -196,8 +196,7 @@ unknown_resources_ok_test(_) ->
     _.
 cant_authorize_without_resource_access(_) ->
     {ok, Token} = issue_token(#{}, unlimited),
-    {error, R} = uac:authorize_api_key(<<"Bearer ", Token/binary>>, #{should_authorize_operations => true}),
-    ct:log("R: ~p", [R]).
+    {error, {invalid_token,{missing,acl}}} = uac:authorize_api_key(<<"Bearer ", Token/binary>>, #{should_authorize_operations => true}).
 
 -spec unknown_resources_fail_encode_test(config()) ->
     _.
